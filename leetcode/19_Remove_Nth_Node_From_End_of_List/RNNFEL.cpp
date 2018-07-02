d43727436671ac1cbda9e8ec4db17fae8e235dd1
MarkTsai <Mark.Tsai@quantatw.com>
2018-07-02 10:39:24 +0800

    [leetcode] 19_Remove_Nth_Node_From_End_of_List
    	tags : Linked List, fast-slow pointer

---
+++ leetcode/19_Remove_Nth_Node_From_End_of_List/RNNFEL.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "LinkListTools.h"
+
+using namespace std;
+
+
+class Solution {
+public:
+    static ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode *dummy = new ListNode(-1);
+		dummy->next = head;
+		ListNode *pre, *cur;
+		pre = dummy;
+		cur = head;
+
+		for (int i = 0; i < n; i++) {
+			cur = cur->next;
+		}
+
+		while (cur) {
+			cur = cur->next;
+			pre = pre->next;
+		}
+		cout << pre->next->val << endl;
+		pre->next = pre->next->next;	//	pre->next is Nth.
+		return dummy->next;
+    }
+};
+
+int main()
+{
+	int ary[] = {5, 4, 3, 2, 1};
+	ListNode *head = createListFromIntAarry(ary, lengthArray(ary));
+	printList(head);
+
+	ListNode *head2 = Solution::removeNthFromEnd(head, 2);
+	printList(head2);
+
+	return 0;
+}
