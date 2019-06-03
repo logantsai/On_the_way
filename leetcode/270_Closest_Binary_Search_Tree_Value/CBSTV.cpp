df8661e40d8dd7471f81e57d195de21d371c5971
MarkTsai <marktsai0213@gmail.com>
2019-06-03 18:33:33 +0800

    [leetcode] 270_Closest_Binary_Search_Tree_Value
        tags : BST

---
+++ leetcode/270_Closest_Binary_Search_Tree_Value/CBSTV.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+#include <TreeTools.h>
+using namespace std;
+
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int a = root->val;
+        TreeNode *t = target < a ? root->left : root->right;
+        if (!t) return a;
+        int b = closestValue(t, target);
+        return abs(a - target) < abs(b - target) ? a : b;
+    }
+};
+
